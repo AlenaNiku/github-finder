4c7f3028a3f6c1103e0c358590ea9a2079b3ea99
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-22 13:21:56 -0400

    added search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
-import UserCard from './UserCard'
+import UserCard from './UserCard';
+import { Button, Form } from "semantic-ui-react";
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [userName, setUserName] = useState('')
+  const [avatar, setAvatar] = useState('')
+  const [userInput, setUserInpu] = useState('')
+
+
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <>
+      <div className="navbar">Search Github User</div>
+
+      <div className="search">
+        <Form>
+          <Form.Field>
+            <label>First Name</label>
+            <input placeholder="First Name" />
+          </Form.Field>
+          
+          <Button type="submit">Search</Button>
+        </Form>
+      </div>
+
       <UserCard />
-    </div>
+    </>
   );
 }
 
