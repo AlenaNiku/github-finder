bb5c97e8bda37e1ab4a929e1490405d53f41d17d
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-22 15:34:57 -0400

    added a line before username

--- src/UserCard.js
+++ src/UserCard.js
@@ -2,25 +2,21 @@ import React from "react";
 import { Card, Image } from "semantic-ui-react";
 
 const CardExampleCard = ({ avatar, name, location, userName, bio }) => (
-  
   <div className="card">
     <Card>
-        <Image
-        src={avatar}
-        wrapped
-        ui={false}
-        />
-        <Card.Content>
-            <Card.Header>{name}</Card.Header>
-            <Card.Header>{bio}</Card.Header>
+      <Image src={avatar} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Header>{bio}</Card.Header>
         <Card.Meta>
-            <span className="date">Located in {location}</span>
+          <span className="date" i class="fa fa-map-marker">
+            {location}
+          </span>
         </Card.Meta>
-        <Card.Description>
-            {userName}
-        </Card.Description>
-        </Card.Content>
-    
+      </Card.Content>
+      <Card.Content extra>
+        <Card.Description>{userName}</Card.Description>
+      </Card.Content>
     </Card>
   </div>
 );
